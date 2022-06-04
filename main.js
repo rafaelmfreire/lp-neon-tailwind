e46309289ff58b092fc4e7d047e87d63d0f955dc
Rafael de Macêdo <r4faelmf@gmail.com>
2022-06-04 15:04:54 -0300

    Scripts movidos para o arquivo main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-import './style.css'
+import "./style.css";
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.3,
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    610: {
+      slidesPerView: 1.75,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 2.3,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
